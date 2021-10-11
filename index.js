faa78eabf14e330057122956d659e4265e83c3c3
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-10-10 17:28:39 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+        alert('I was clicked again!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener();
